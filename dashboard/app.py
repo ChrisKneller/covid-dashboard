18bf610fee823739baa780cba7e61b757ead81b0
Chris <christian.kneller@gmail.com>
2020-04-01 18:55:18 +0100

    Added grid layout, separate graphs, better source of information

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,13 +1,20 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from data import df
+from data import df1, df2, filter_df
 import plotly.graph_objects as go
 import datetime
+import numpy as np
+import dash_ui as dui
 
 # print(df)
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    'https://codepen.io/rmarren1/pen/mLqGRg.css'
+    ]
+
+grid = dui.Grid(_id="grid", num_rows=12, num_cols=12, grid_padding=0)
 
 def generate_table(dataframe, max_rows=999):
     return html.Table([
@@ -52,7 +59,7 @@ def generate_map(df):
             name=location_name + " - confirmed",
             marker=dict(
                 size=int(df.iloc[location][8]**(0.5))/10 + 5,
-                color='rgb(102, 153, 255)',
+                color='rgba(102, 153, 255, 0.8)',
                 line_color='rgb(40,40,40)',
                 line_width=0,
             )
@@ -66,7 +73,7 @@ def generate_map(df):
             name=location_name + " - deaths",
             marker=dict(
                 size=int(df.iloc[location][9]**(0.5))/10 + 2,
-                color='rgb(255, 102, 102)',
+                color='rgba(255, 102, 102, 0.8)',
                 line_color='rgb(40,40,40)',
                 line_width=0,
             )
@@ -74,10 +81,108 @@ def generate_map(df):
     return fig
 
 
+def generate_map_w_options(df, plot_cases=True, plot_recoveries=True, plot_deaths=True):
+    
+    latest_date = df.iloc[len(df)-1][2]
+    df = filter_df(df,"Date",latest_date)
+    
+    fig = go.Figure(go.Scattergeo())
+    fig.update_geos(
+        projection_type="natural earth",
+        showcountries=True,
+        countrycolor='rgb(40,40,40)'
+        )
+    
+    titletext = "Confirmed"
+    if plot_cases:
+        titletext += " cases"
+    if plot_recoveries:
+        titletext += " recoveries"
+    if plot_deaths:
+        titletext += " deaths"
+    titletext += (" " + latest_date)
+    
+    fig.update_layout(
+        title={
+            'text': titletext,
+            'y': 0.95,
+            'x': 0.5,
+            'xanchor': 'center',
+            },
+        height=500, 
+        margin={"r":5,"t":10,"l":5,"b":0},
+        showlegend=False,
+        )
+
+    for location in range((len(df))):
+        # Sort out the naming of each plot point 
+        # (some are just countries, some are country provinces)
+        has_province = True if df.iloc[location][6] else False
+        
+        location_name = df.iloc[location][1]
+        if has_province:
+            location_name += f" ({df.iloc[location][6]})"
+
+        if plot_cases:
+            # Plot confirmed cases in each area
+            confirmed = 0 if np.isnan(df.iloc[location][0]) else df.iloc[location][0]
+            fig.add_trace(go.Scattergeo(
+                lon=[float(df.iloc[location][5])],
+                lat=[df.iloc[location][4]],
+                text=f"{location_name}: {confirmed:,.0f} confirmed",
+                name=location_name + " - confirmed",
+                marker=dict(
+                    size=int(confirmed**(0.5))/10,
+                    color='rgba(102, 153, 255, 0.8)',
+                    line_color='rgb(40,40,40)',
+                    line_width=0,
+                )
+            ))
+
+        if plot_deaths:
+            # Plot deaths in each area
+            deaths = 0 if np.isnan(df.iloc[location][3]) else df.iloc[location][3]
+            if deaths == 0:
+                continue
+            fig.add_trace(go.Scattergeo(
+                lon=[float(df.iloc[location][5])],
+                lat=[df.iloc[location][4]],
+                text=f"{location_name}: {deaths:,.0f} dead",
+                name=location_name + " - deaths",
+                marker=dict(
+                    size=int(deaths**(0.5))/10,
+                    color='rgba(255, 102, 102, 0.8)',
+                    line_color='rgb(40,40,40)',
+                    line_width=0,
+                )
+            ))
+
+        if plot_recoveries:
+        # Plot recoveries in each area
+            recoveries = 0 if np.isnan(df.iloc[location][7]) else df.iloc[location][7]
+            if recoveries == 0:
+                continue
+            fig.add_trace(go.Scattergeo(
+                lon=[float(df.iloc[location][5])],
+                lat=[df.iloc[location][4]],
+                text=f"{location_name}: {recoveries:,.0f} recovered",
+                name=location_name + " - recoveries",
+                marker=dict(
+                    size=int(recoveries**(0.5))/10,
+                    color='rgba(52, 189, 45, 0.8)',
+                    line_color='rgb(40,40,40)',
+                    line_width=0,
+                )
+            ))
+
+    return fig
+
+
+
 def generate_horizontal_bar(df, max_rows=50):
     top_labels = ['Death rate']
-    # colours = ['rgb(204, 204, 255)',] * max_rows
     colours = []    
+    # Get a list of colours that change as you change values being plotted
     for row in range(max_rows):
         colours.append(f'rgb(255,{194/max_rows*(max_rows-row)+92},{153/max_rows*(max_rows-row)})')
     
@@ -116,7 +221,7 @@ def generate_horizontal_bar(df, max_rows=50):
     y_data.append("Average")
     y_label.append(f"{average_death_rate*100:.1f}% ({total_deaths:,} deaths, {total_cases:,} cases)")
 
-    # Sort the two axes
+    # Sort the axes
     x_data, y_data, y_label = (list(t) for t in zip(*sorted(zip(x_data, y_data, y_label))))
 
     # Trim data to the amount of max_rows specified
@@ -147,6 +252,7 @@ def generate_horizontal_bar(df, max_rows=50):
     fig.update_xaxes(range=[0, 1])
 
     fig.update_layout(
+        title="Plot Title",
         barmode='stack',
         margin={"r":0,"t":0,"l":0,"b":0},
         height=18*max_rows,
@@ -161,46 +267,108 @@ def generate_horizontal_bar(df, max_rows=50):
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
+# Use this if not using grid layout
+# app.layout = html.Div(children=[
 
-    html.H1(children='Covid-19 tracker'),
-    html.H2(children='World map'),
-    dcc.Graph(
-        id='World map',
-        config={
-            "displaylogo": False,
-        },
-        figure=generate_map(df)
-        ),
+#     html.H1(children='Covid-19 tracker'),
+#     html.H2(children='World map'),
+#     dcc.Graph(
+#         id='World map',
+#         config={
+#             "displaylogo": False,
+#         },
+#         figure=generate_map(df1)
+#         ),
 
-    # html.Div(children='''
-    #     Dash: A web application framework for Python.
-    # '''),
+#     dcc.Graph(
+#         id='World map with recoveries',
+#         config={
+#             "displaylogo": False,
+#         },
+#         figure=generate_map_w_recoveries(df2)
+#         ),
 
-    # dcc.Graph(
-    #     id='example-graph',
-    #     figure={
-    #         'data': [
-    #             {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-    #             {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-    #         ],
-    #         'layout': {
-    #             'title': 'Dash Data Visualization'
-    #         }
-    #     }
-    # ),
-    html.H2(children='Death rates'),
-    dcc.Graph(
-        id="Death rates",
-        config={
-            "displaylogo": False,
-        },
-        figure=generate_horizontal_bar(df),
+#     html.H2(children='Death rates'),
+#     dcc.Graph(
+#         id="Death rates",
+#         config={
+#             "displaylogo": False,
+#         },
+#         figure=generate_horizontal_bar(df1),
+#     ),
+
+#     html.H2(children='Raw data'),
+#     generate_table(df1)
+# ])
+
+# grid.add_element(col=1, row=1, width=4, height=4, element=html.Div(children=[
+#     html.H5(children="Confirmed", style={"height": "8%"}),
+#     dcc.Graph(
+#         id='World map of confirmed cases',
+#         config={
+#             "displaylogo": False,
+#         },
+#         figure=generate_map_w_options(df2, plot_recoveries=False, plot_deaths=False),
+#         style={"height": "80%"}
+#     )
+#     ],
+#     # style={"height": "100%", "width": "100%"}
+# ))
+
+grid.add_element(col=1, row=1, width=4, height=4, element=dcc.Graph(
+    id='World map of confirmed cases',
+    config={
+        "displaylogo": False,
+    },
+    figure=generate_map_w_options(df2, plot_recoveries=False, plot_deaths=False),
+    style={"height": "100%", "width": "100%"}
+))
+
+grid.add_element(col=5, row=1, width=4, height=4, element=dcc.Graph(
+    id='World map of confirmed recoveries',
+    config={
+        "displaylogo": False,
+    },
+    figure=generate_map_w_options(df2, plot_cases=False, plot_deaths=False),
+    style={"height": "100%", "width": "100%"}
+))
+
+grid.add_element(col=9, row=1, width=4, height=4, element=dcc.Graph(
+    id='World map of confirmed deaths',
+    config={
+        "displaylogo": False,
+    },
+    figure=generate_map_w_options(df2, plot_cases=False, plot_recoveries=False),
+    style={"height": "100%", "width": "100%"}
+))
+
+grid.add_element(col=1, row=5, width=12, height=6, element=dcc.Graph(
+    id="Death rates",
+    config={
+        "displaylogo": False,
+    },
+    figure=generate_horizontal_bar(df1),
+    style={"background-color": "green", "height": "100%", "width": "100%"}
+))
+
+grid.add_element(col=1, row=9, width=9, height=2, element=html.Div(
+    style={"background-color": "orange", "height": "100%", "width": "100%"}
+))
+
+grid.add_element(col=10, row=9, width=3, height=2, element=html.Div(
+    style={"background-color": "purple", "height": "100%", "width": "100%"}
+))
+
+app.layout = html.Div(
+    dui.Layout(
+        grid=grid,
     ),
+    style={
+        'height': '100vh',
+        'width': '100vw'
+    }
+)
 
-    html.H2(children='Raw data'),
-    generate_table(df)
-])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- dashboard/data.py
+++ dashboard/data.py
@@ -1,13 +1,6 @@
 import requests
 import pandas as pd
 
-BASE_API_URL = 'https://coronavirus-tracker-api.herokuapp.com/v2/'
-
-locations = BASE_API_URL + 'locations'
-
-r = requests.get(locations)
-
-mydata = r.json()
 
 class Country:
     def __init__(self, data):
@@ -20,16 +13,27 @@ class Country:
     def __str__(self):
         return self.country
 
-df = pd.json_normalize(mydata['locations'])
 
-# country_data = []
+def filter_df(df,column,value):
+    return df.loc[df[column] == value]
 
-# for i in mydata['locations']:
-#     country_object = Country(i)
-#     country_data.append(country_object)
+# DATA FROM SOURCE 1
+BASE_API_URL = 'https://coronavirus-tracker-api.herokuapp.com/v2/'
+locations = BASE_API_URL + 'locations'
+r = requests.get(locations)
+s1_data = r.json()
+df1 = pd.json_normalize(s1_data['locations'])
 
-# print(country_data)
+# DATA FROM SOURCE 2
+INFO_URL = 'https://datahub.io/core/covid-19/datapackage.json'
+# AGGREGRATED_COUNTRIES_JSON = 'https://pkgstore.datahub.io/core/covid-19/countries-aggregated_json/data/54fd5d25097dbb94f12e6724473b3fc7/countries-aggregated_json.json'
+# COMBINED_JSON = 'https://pkgstore.datahub.io/core/covid-19/time-series-19-covid-combined_json/data/698009a8089880f873ec98c967e12fb3/time-series-19-covid-combined_json.json'
+r = requests.get(INFO_URL)
 
-# print(len(data['locations']))
-# print(data['locations'][1])
+for i in range(len(r.json()['resources'])):
+    if r.json()['resources'][i]['name'] == 'time-series-19-covid-combined_json':
+        CURRENT_API = r.json()['resources'][i]['path']
 
+r = requests.get(CURRENT_API)
+s2_data = r.json()
+df2 = pd.json_normalize(r.json())
