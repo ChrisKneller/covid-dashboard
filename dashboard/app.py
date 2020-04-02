934aaec84f2b69b152471ea09e6d7c3d56edc44e
Chris <christian.kneller@gmail.com>
2020-04-02 15:52:18 +0100

    Added time series graph & more data sources

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,8 +1,9 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from data import df1, df2, filter_df
+from data import df1, df2, filter_df, resources, df_from_path
 import plotly.graph_objects as go
+import plotly.express as px
 import datetime
 import numpy as np
 import dash_ui as dui
@@ -81,8 +82,8 @@ def generate_map(df):
     return fig
 
 
+# Create a world map and plot cases, recoveries and/or deaths
 def generate_map_w_options(df, plot_cases=True, plot_recoveries=True, plot_deaths=True):
-    
     latest_date = df.iloc[len(df)-1][2]
     df = filter_df(df,"Date",latest_date)
     
@@ -112,6 +113,10 @@ def generate_map_w_options(df, plot_cases=True, plot_recoveries=True, plot_death
         height=500, 
         margin={"r":5,"t":10,"l":5,"b":0},
         showlegend=False,
+        font=dict(
+            family="Courier New, monospace",
+            size=12,
+        ),
         )
 
     for location in range((len(df))):
@@ -178,8 +183,10 @@ def generate_map_w_options(df, plot_cases=True, plot_recoveries=True, plot_death
     return fig
 
 
-
-def generate_horizontal_bar(df, max_rows=50):
+# Plot a horizontal bar chat showing death rates
+def generate_horizontal_bar(df, max_rows=30,cases_cutoff=100):
+    if df.empty():
+        return None
     top_labels = ['Death rate']
     colours = []    
     # Get a list of colours that change as you change values being plotted
@@ -206,10 +213,11 @@ def generate_horizontal_bar(df, max_rows=50):
             cases+=1
         deaths = df.iloc[location][9]
 
-        death_rate = deaths/cases
-        x_data.append(death_rate)
-        y_data.append(location_name)
-        y_label.append(f"{death_rate*100:.1f}% ({deaths:,} deaths, {cases:,} cases)")
+        if cases > cases_cutoff:
+            death_rate = deaths/cases
+            x_data.append(death_rate)
+            y_data.append(location_name)
+            y_label.append(f"{death_rate*100:.1f}% ({deaths:,}/{cases:,})")
 
     # Sum the total cases & total deaths, calc avg death rate
     total_cases = df.sum(axis=0)[8]
@@ -219,15 +227,16 @@ def generate_horizontal_bar(df, max_rows=50):
     # Add avg death rate to dataset
     x_data.append(average_death_rate)
     y_data.append("Average")
-    y_label.append(f"{average_death_rate*100:.1f}% ({total_deaths:,} deaths, {total_cases:,} cases)")
+    y_label.append(f"{average_death_rate*100:.1f}% ({total_deaths:,}/{total_cases:,})")
 
     # Sort the axes
     x_data, y_data, y_label = (list(t) for t in zip(*sorted(zip(x_data, y_data, y_label))))
 
     # Trim data to the amount of max_rows specified
-    x_data = x_data[len(x_data)-max_rows:len(x_data)]
-    y_data = y_data[len(y_data)-max_rows:len(y_data)]
-    y_label = y_label[len(y_label)-max_rows:len(y_label)]
+    if max_rows > len(x_data):
+        x_data = x_data[len(x_data)-max_rows:len(x_data)]
+        y_data = y_data[len(y_data)-max_rows:len(y_data)]
+        y_label = y_label[len(y_label)-max_rows:len(y_label)]
 
     # Change the colour of the average item
     colours[y_data.index('Average')] = 'red'
@@ -252,17 +261,172 @@ def generate_horizontal_bar(df, max_rows=50):
     fig.update_xaxes(range=[0, 1])
 
     fig.update_layout(
-        title="Plot Title",
+        title=f"Worst death rates ({cases_cutoff}+ cases)",
         barmode='stack',
-        margin={"r":0,"t":0,"l":0,"b":0},
+        margin={"r":0,"t":30,"l":0,"b":0},
         height=18*max_rows,
         xaxis=dict(
-            tickformat=".1%"
+            tickformat=".1%",
+        ),
+        font=dict(
+            family="Courier New, monospace",
+            size=12,
         )
         )
-    return fig
-    
 
+    fig.update_yaxes(tickfont=dict(size=10),)
+    return fig
+
+
+# Plot horizontal bar chart by country level
+def generate_deathrates_by_country(resources=resources, max_rows=30, min_cases=100, date=False):
+    df = df_from_path(resources['countries-aggregated'])
+    
+    # If no date is given, take the latest
+    if not date:
+        date = df.iloc[len(df)-1,0] 
+    df = filter_df(df, "Date", date)
+    
+    # Set up for the data
+    x_data = []
+    y_data = []
+    y_label = []
+
+    # Gather the data from the dataframe
+    for location in range((len(df))):            
+        location_name = df.iloc[location][1]
+
+
+        cases = df.iloc[location][2]
+        if cases == 0:
+            cases+=1
+        deaths = df.iloc[location][4]
+
+        if cases > min_cases:
+            death_rate = deaths/cases
+            x_data.append(death_rate)
+            y_data.append(location_name)
+            y_label.append(f"{death_rate*100:.1f}% ({deaths:,}/{cases:,})")
+
+    num_rows = min(max_rows, len(x_data))
+
+    # Get a list of colours that change as you change values being plotted
+    colours = []
+    for row in range(num_rows+1):
+        colours.append(f'rgb(255,{194/num_rows*(num_rows-row)+92},{153/num_rows*(num_rows-row)+20})')
+
+    # Sum the total cases & total deaths, calc avg death rate
+    total_cases = df.sum(axis=0)[2]
+    total_deaths = df.sum(axis=0)[4]
+    average_death_rate = total_deaths / total_cases
+
+    # Add avg death rate to dataset
+    x_data.append(average_death_rate)
+    y_data.append("Average")
+    y_label.append(f"{average_death_rate*100:.1f}% ({total_deaths:,}/{total_cases:,})")
+
+    # Sort the axes
+    x_data, y_data, y_label = (list(t) for t in zip(*sorted(zip(x_data, y_data, y_label))))
+
+    # Trim data to the amount of max_rows specified
+    x_data = x_data[len(x_data)-max_rows:len(x_data)]
+    y_data = y_data[len(y_data)-max_rows:len(y_data)]
+    y_label = y_label[len(y_label)-max_rows:len(y_label)]
+
+    # Change the colour of the average item
+    try:
+        colours[y_data.index('Average')] = 'red'
+    except:
+        print(f"Average not in top {num_rows}")
+
+    # Plot the graph
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=x_data,
+        y=y_data,
+        text=y_label,
+        textposition="outside",
+        name="Death rate summary",
+        orientation='h',
+        marker=dict(
+            color=colours,
+            line=dict(
+                color='rgba(38, 24, 74, 0.8)',
+                width=1)
+        )
+    ))
+
+    fig.update_xaxes(range=[0, 1])
+
+    fig.update_layout(
+        title={
+            'text': f"Death rates by country ({min_cases}+ cases)",
+            'x': 0.5,
+            'xanchor': 'center',
+        },
+        barmode='stack',
+        margin={"r":0,"t":30,"l":0,"b":0},
+        height=18*num_rows,
+        xaxis=dict(
+            tickformat=".1%",
+        ),
+        font=dict(
+            family="Courier New, monospace",
+            size=12,
+        )
+        )
+
+    fig.update_yaxes(tickfont=dict(size=12),)
+    return fig
+
+# Plot a time series graph from the worldwide data
+def generate_world_time_series(resources=resources):
+    df = df_from_path(resources['worldwide-aggregated']) 
+    fig = px.line(df, x='Date', y='Confirmed')
+    # fig = px.line(df, x='Date', y='Recovered')
+    # fig = px.line(df, x='Date', y='Deaths')
+    return fig
+
+
+def generate_world_ts_options(resources=resources, plot_confirmed=True, plot_recovered=True, plot_deaths=True):
+    df = df_from_path(resources['worldwide-aggregated']) 
+    # Add data
+    date = df['Date'].tolist()
+    confirmed = df['Confirmed'].tolist()
+    recovered = df['Recovered'].tolist()
+    deaths = df['Deaths'].tolist()
+
+    fig = go.Figure()
+
+    # Create and style traces
+    if plot_confirmed:
+        fig.add_trace(go.Scatter(x=date, y=confirmed, name='Cases',
+                                line=dict(color='rgba(102, 153, 255, 0.8)', width=2)))
+    if plot_recovered:
+        fig.add_trace(go.Scatter(x=date, y=recovered, name = 'Recoveries',
+                                line=dict(color='rgba(52, 189, 45, 0.8)', width=2)))
+    if plot_deaths:
+        fig.add_trace(go.Scatter(x=date, y=deaths, name='Deaths',
+                                line=dict(color='rgba(255, 102, 102, 0.8)', width=2)))
+
+    # Edit the layout
+    fig.update_layout(
+        title={
+            'text': f"Global confirmed numbers over time",
+            'x': 0.5,
+            'xanchor': 'center',
+        },
+        font=dict(
+            family="Courier New, monospace",
+            size=12,
+        ),
+        xaxis_title='Date',
+        yaxis_title='Confirmed numers',
+        hovermode='x')
+
+    fig.update_yaxes(nticks=10)
+
+    return fig
 
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -342,17 +506,26 @@ grid.add_element(col=9, row=1, width=4, height=4, element=dcc.Graph(
     style={"height": "100%", "width": "100%"}
 ))
 
-grid.add_element(col=1, row=5, width=12, height=6, element=dcc.Graph(
+grid.add_element(col=1, row=5, width=4, height=4, element=dcc.Graph(
     id="Death rates",
     config={
         "displaylogo": False,
     },
-    figure=generate_horizontal_bar(df1),
-    style={"background-color": "green", "height": "100%", "width": "100%"}
+    figure=generate_deathrates_by_country(max_rows=500),
+    style={"width": "100%",}
 ))
 
-grid.add_element(col=1, row=9, width=9, height=2, element=html.Div(
-    style={"background-color": "orange", "height": "100%", "width": "100%"}
+grid.add_element(col=5, row=5, width=8, height=4, element=html.Div(
+    style={"background-color": "red", "height": "100%", "width": "100%"}
+))
+
+grid.add_element(col=1, row=9, width=9, height=4, element=dcc.Graph(
+    id="Overall time series",
+    config={
+        "displaylogo": False,
+    },
+    figure=generate_world_ts_options(),
+    style={"height": "100%", "width": "100%"}
 ))
 
 grid.add_element(col=10, row=9, width=3, height=2, element=html.Div(
--- dashboard/data.py
+++ dashboard/data.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import io
 
 
 class Country:
@@ -17,22 +18,37 @@ class Country:
 def filter_df(df,column,value):
     return df.loc[df[column] == value]
 
-# DATA FROM SOURCE 1
-BASE_API_URL = 'https://coronavirus-tracker-api.herokuapp.com/v2/'
-locations = BASE_API_URL + 'locations'
-r = requests.get(locations)
-s1_data = r.json()
-df1 = pd.json_normalize(s1_data['locations'])
+try:
+    # DATA FROM SOURCE 1
+    BASE_API_URL = 'https://coronavirus-tracker-api.herokuapp.com/v2/'
+    locations = BASE_API_URL + 'locations'
+    r = requests.get(locations)
+    s1_data = r.json()
+    df1 = pd.json_normalize(s1_data['locations'])
+except:
+    df1 = 0
 
 # DATA FROM SOURCE 2
 INFO_URL = 'https://datahub.io/core/covid-19/datapackage.json'
-# AGGREGRATED_COUNTRIES_JSON = 'https://pkgstore.datahub.io/core/covid-19/countries-aggregated_json/data/54fd5d25097dbb94f12e6724473b3fc7/countries-aggregated_json.json'
-# COMBINED_JSON = 'https://pkgstore.datahub.io/core/covid-19/time-series-19-covid-combined_json/data/698009a8089880f873ec98c967e12fb3/time-series-19-covid-combined_json.json'
 r = requests.get(INFO_URL)
+
+resources = {}
 
 for i in range(len(r.json()['resources'])):
     if r.json()['resources'][i]['name'] == 'time-series-19-covid-combined_json':
         CURRENT_API = r.json()['resources'][i]['path']
+    res_name = r.json()['resources'][i]['name']
+    res_path = r.json()['resources'][i]['path']
+    resources[res_name] = res_path
+
+def df_from_path(path):
+    if "csv" in path:
+        new_r = requests.get(path).content
+        df = pd.read_csv(io.StringIO(new_r.decode('utf-8')))
+    elif "json" in path:
+        new_r = requests.get(path)
+        df = pd.json_normalize(new_r.json())
+    return df
 
 r = requests.get(CURRENT_API)
 s2_data = r.json()
